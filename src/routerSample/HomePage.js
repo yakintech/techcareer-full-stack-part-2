328ba30966e1ee02a9374329194163ae651689b1
cagatayyildiz <yildiz.cagatay@hotmail.com>
2022-10-06 17:59:57 +0300

    ref hook and build folder

--- src/routerSample/HomePage.js
+++ src/routerSample/HomePage.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 
 function HomePage() {
-  return (
-    <div>HomePage</div>
+
+  const [pColor, setcolor] = useState('');
+
+  let pRef = useRef();
+
+  const changeColor = () => {
+    // setcolor('tomato')
+
+    // document.getElementById('blog').style.color = 'tomato';
+    pRef.current.style.color = 'brown'
+
+  }
+
+
+  
+
+  return (<>
+  
+  <p ref={pRef} id='blog' style={{color:pColor}}>Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Curabitur non nulla sit amet nisl tempus convallis</p>
+
+  <button onClick={() => changeColor()}>Change Color</button>
+  <button onClick={() => window.hello()}>Hello</button>
+  </>
   )
 }
 
